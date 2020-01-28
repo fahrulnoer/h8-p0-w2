3af73732c6302ae293c6427b32a89732256eae99
fahrulnoer <fahrulnoer17@gmail.com>
2020-01-29 00:15:42 +0700

    add exercise 4

---
+++ exercise-4.js
@@ -0,0 +1,51 @@
+// Nama : Fahrul Muhammad Noer
+// exercise 4 week 2 phase 0
+
+// var tanggal; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+// var bulan; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+// var tahun; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+
+var tanggal = 30;
+var bulan = 2;
+var tahun = 1999;
+
+if(tanggal < 1 || tanggal > 31){
+    console.log('Perbaiki input tanggal!')
+    return 
+}else if( tahun < 1900 || tahun > 2300) {
+    console.log('Perbaiki input tahun!')
+    return 
+}else if(bulan == 2){
+    if(tanggal > 28){
+        console.log("bulan Februari hanya 28 hari")
+        return 
+    }
+}
+switch(bulan) {
+    case 1 : console.log(`${tanggal} Januari ${tahun}`);
+    break;
+    case 2 : console.log(`${tanggal} Februari ${tahun}`);
+    break;
+    case 3 : console.log(`${tanggal} Maret ${tahun}`);
+    break;
+    case 4 : console.log(`${tanggal} April ${tahun}`);
+    break;
+    case 5 : console.log(`${tanggal} Mei ${tahun}`);
+    break;
+    case 6 : console.log(`${tanggal} Juni ${tahun}`);
+    break;
+    case 7 : console.log(`${tanggal} Juli ${tahun}`);
+    break;
+    case 8 : console.log(`${tanggal} Agustus ${tahun}`);
+    break;
+    case 9 : console.log(`${tanggal} September ${tahun}`);
+    break;
+    case 10 : console.log(`${tanggal} Oktober ${tahun}`);
+    break;
+    case 11 : console.log(`${tanggal} November ${tahun}`);
+    break;
+    case 12 : console.log(`${tanggal} Desember ${tahun}`);
+    break;
+    default : console.log("Perbaiki input bulan!")
+    break;
+}
